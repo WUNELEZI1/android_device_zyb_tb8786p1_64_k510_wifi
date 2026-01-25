8c9ca32a5b833552eed62768fad95cb3531af6e1
WUNELEZI1 <18733329732@163.com>
2026-01-25 19:16:50 +0800

    fuck zyb

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-# tb8786p1_64_k510_wifi Android.mk
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter tb8786p1_64_k510_wifi, $(TARGET_DEVICE)),)
@@ -6,23 +5,7 @@ ifneq ($(filter tb8786p1_64_k510_wifi, $(TARGET_DEVICE)),)
 # 优先加载配置
 include $(call all-subdir-makefiles)
 
-# 核心模块（编译vendor_boot/boot，无独立recovery）
-PRODUCT_PACKAGES += \
-    vendor_bootimage \
-    bootimage
-
-# 设备属性（匹配解包的build.prop）
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.device=tb8786p1_64_k510_wifi \
-    ro.product.name=vnd_tb8786p1_64_k510_wifi \
-    ro.product.brand=ZYB \
-    ro.product.model=ZPD1321 \
-    ro.product.manufacturer=ZYB \
-    ro.bootimage.build.id=SP1A.210812.016 \
-    ro.bootimage.build.version.sdk=31 \
-    ro.bootimage.build.version.release=12
-
-# 复制预编译文件（使用你已有的内核/dtb）
+# 复制预编译文件（使用已有的内核/dtb）
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/Image.gz-dtb:kernel \
     $(LOCAL_PATH)/dtb/mt6768.dtb:dtb.img \
--- omni_tb8786p1_64_k510_wifi.mk
+++ omni_tb8786p1_64_k510_wifi.mk
@@ -3,11 +3,21 @@ $(call inherit-product, device/zyb/tb8786p1_64_k510_wifi/BoardConfig.mk)
 $(call inherit-product, device/zyb/tb8786p1_64_k510_wifi/twrp.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# 合并属性声明，避免冲突
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.twrp.version=3.7.0 \
+    ro.twrp.version=3.7.0-zyb \
     ro.twrp.screen_width=1200 \
     ro.twrp.screen_height=1920 \
-    ro.twrp.theme=portrait_hdpi
+    ro.twrp.theme=portrait_hdpi \
+    ro.twrp.build.date=$(shell date +%Y%m%d) \
+    ro.product.device=tb8786p1_64_k510_wifi \
+    ro.product.name=vnd_tb8786p1_64_k510_wifi \
+    ro.product.brand=ZYB \
+    ro.product.model=ZPD1321 \
+    ro.product.manufacturer=ZYB \
+    ro.bootimage.build.id=SP1A.210812.016 \
+    ro.bootimage.build.version.sdk=31 \
+    ro.bootimage.build.version.release=12
 
 PRODUCT_NAME := omni_tb8786p1_64_k510_wifi
 PRODUCT_DEVICE := tb8786p1_64_k510_wifi
