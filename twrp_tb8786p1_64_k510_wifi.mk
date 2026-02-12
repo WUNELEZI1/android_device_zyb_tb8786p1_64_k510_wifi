8a419271673b5b819562e4fd11d2b11575a0a41c
WUNELEZI1 <18733329732@163.com>
2026-02-12 19:47:57 +0800

    fuck zyb

--- twrp_tb8786p1_64_k510_wifi.mk
+++ twrp_tb8786p1_64_k510_wifi.mk
@@ -3,6 +3,8 @@ TW_SCREEN_WIDTH := 1200
 TW_SCREEN_HEIGHT := 1920
 TW_BUILD_VENDOR_BOOT := true
 TW_NO_BUILD_RECOVERY_IMAGE := false
+TW_SUPPORT_VENDOR_BOOT := true
+TW_SUPPORT_BOOT_HEADER_V4 := true  # 添加这个
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/zyb/tb8786p1_64_k510_wifi/BoardConfig.mk)
@@ -15,16 +17,16 @@ PRODUCT_BRAND := ZYB
 PRODUCT_MODEL := ZPD1321
 PRODUCT_MANUFACTURER := ZYB
 
-# ========== 版本配置 ==========
-PRODUCT_PLATFORM_VERSION := 12
-PRODUCT_PLATFORM_SDK_VERSION := 31
-PRODUCT_PLATFORM_VERSION_LAST_STABLE := 12
-PRODUCT_SHIPPING_API_LEVEL := 31
+# ========== 版本配置 (适配 TWRP-14/Android 14) ==========
+PRODUCT_PLATFORM_VERSION := 14
+PRODUCT_PLATFORM_SDK_VERSION := 34
+PRODUCT_PLATFORM_VERSION_LAST_STABLE := 14
+PRODUCT_SHIPPING_API_LEVEL := 34
 PRODUCT_SECURITY_PATCH := 2024-09-05
 PRODUCT_VENDOR_SECURITY_PATCH := 2024-09-05
 
-# [!重要!] 只有一个 BUILD_FINGERPRINT 定义，没有空格！
-PRODUCT_BUILD_FINGERPRINT := ZYB/vnd_tb8786p1_64_k510_wifi/tb8786p1_64_k510_wifi:12/SP1A.210812.016/20240905:user/release-keys
+# 构建指纹 - Android 14 格式
+PRODUCT_BUILD_FINGERPRINT := ZYB/vnd_tb8786p1_64_k510_wifi/tb8786p1_64_k510_wifi:14/UP1A.240905.001/20240905:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.twrp.version=3.7.0-zyb \
@@ -34,16 +36,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.brand=ZYB \
     ro.product.model=ZPD1321 \
     ro.product.manufacturer=ZYB \
-    ro.bootimage.build.id=SP1A.210812.016 \
-    ro.bootimage.build.version.sdk=31 \
-    ro.bootimage.build.version.release=12 \
+    ro.bootimage.build.id=UP1A.240905.001 \
+    ro.bootimage.build.version.sdk=34 \
+    ro.bootimage.build.version.release=14 \
     ro.build.version.security_patch=2024-09-05 \
     ro.build.fingerprint=$(PRODUCT_BUILD_FINGERPRINT) \
     ro.sf.lcd_density=320 \
     ro.zygote=zygote64_32 \
     ro.dynamic_partitions=true \
-    ro.vndk.version=31 \
-    ro.first_api_level=31
+    ro.vndk.version=34 \
+    ro.first_api_level=34
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/Image.gz-dtb:kernel \
@@ -51,14 +53,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab \
     $(LOCAL_PATH)/dynamic_partitions_opts.xml:dynamic_partitions_opts.xml
 
-# [!重要!] 删除多余的 BUILD_FINGERPRINT 和其他重复定义！
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=tb8786p1_64_k510_wifi \
     PRODUCT_NAME=twrp_tb8786p1_64_k510_wifi \
-    PLATFORM_VERSION=12 \
-    BUILD_ID=SP1A.210812.016 \
+    PLATFORM_VERSION=14 \
+    BUILD_ID=UP1A.240905.001 \
     SECURITY_PATCH=2024-09-05 \
-    FIRST_API_LEVEL=31 \
-    VNDK_VERSION=31 \
+    FIRST_API_LEVEL=34 \
+    VNDK_VERSION=34 \
     ROOT_MANAGER=1 \
     TW_DEVICE_VERSION=3.7.0-zyb
